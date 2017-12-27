8fb20df1e07b2f0f9b1c1b893bd17fedd50a760c
OSCAR <ochng6654@gmail.com>
2017-12-27 15:59:51 +0800

    add logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -33,7 +33,7 @@ Spree.config do |config|
   # Admin:
 
   # Custom logo for the admin
-  # config.admin_interface_logo = "logo/solidus_logo.png"
+  config.admin_interface_logo = "logo/solidus_logo.png"
 
   # Gateway credentials can be configured statically here and referenced from
   # the admin. They can also be fully configured from the admin.
