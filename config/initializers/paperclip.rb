b438b934d81a43702f5ee323a3883a3ae98dec28
OSCAR <ochng6654@gmail.com>
2017-12-28 17:18:07 +0800

    add location for aws storage

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,7 +4,8 @@ if ENV['AWS_ACCESS_KEY_ID']
     :fog_credentials => {
       :provider => "AWS",
       :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => "ap-southeast-1"
     },
     :fog_directory => ENV["S3_BUCKET_NAME"]
   )
